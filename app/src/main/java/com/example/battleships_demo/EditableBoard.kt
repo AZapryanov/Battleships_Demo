499c598e11b57c17e93af1ed32416ba1eb2fdcd9
Chris <83955174+blobleet@users.noreply.github.com>
2021-12-21 16:47:58 +0200

    Update EditableBoard.kt

--- app/src/main/java/com/example/battleships_demo/EditableBoard.kt
+++ app/src/main/java/com/example/battleships_demo/EditableBoard.kt
@@ -3,5 +3,5 @@ package com.example.battleships_demo
 import android.content.Context
 import android.util.AttributeSet
 
-class EditableBoard(context: Context, attrs: AttributeSet) {
+class EditableBoard(context: Context, attrs: AttributeSet) : Board(context, attrs) {
 }
