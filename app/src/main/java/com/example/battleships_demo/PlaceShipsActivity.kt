d34f1ea720e98e365c730f3032f446e14065e6e0
Chris <83955174+blobleet@users.noreply.github.com>
2021-12-22 15:32:22 +0200

    Update PlaceShipsActivity.kt

--- app/src/main/java/com/example/battleships_demo/PlaceShipsActivity.kt
+++ app/src/main/java/com/example/battleships_demo/PlaceShipsActivity.kt
@@ -2,10 +2,16 @@ package com.example.battleships_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.battleships_demo.customviews.EditableBoard
 
 class PlaceShipsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_place_ships)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val boardState = findViewById<EditableBoard>(R.id.editable_board).getBoardState()
     }
 }
